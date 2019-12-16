cac3d65190143bbff6f21330fb3c580189ebc2a0
Marcel Rico <personal.mr4@gmail.com>
2019-12-16 04:05:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    idx = 0
+    while self[idx]
+      puts self[idx]
+    end
   end
 end
